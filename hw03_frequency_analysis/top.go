c4daf546832a0dc5447cd28bf41c8542479e3255
asgor <alexmilesjg@gmail.com>
2022-09-05 17:25:50 +0500

    Тесты

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -5,6 +5,11 @@ import (
 	"strings"
 )
 
+type keyValue struct {
+	Key   string
+	Value int
+}
+
 func Top10(str string) []string {
 	var st []string
 	var pr string
@@ -13,50 +18,50 @@ func Top10(str string) []string {
 
 	if str == "" {
 		return st
-	} else {
-		s := strings.Fields(str)
+	}
+	s := strings.Fields(str)
 
-		sort.Slice(s, func(i, j int) bool {
-			return s[i] < s[j]
-		})
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
 
-		for _, val := range s {
-			if pr == val {
-				cach[val]++
-			} else {
-				cach[val] = 1
-			}
-			pr = val
+	for _, val := range s {
+		if pr == val {
+			cach[val]++
+		} else {
+			cach[val] = 1
 		}
+		pr = val
+	}
 
-		for _, val := range cach {
-			if val > valpr {
-				valpr = val
-			}
+	for _, val := range cach {
+		if val > valpr {
+			valpr = val
 		}
+	}
 
-		type keyValue struct {
-			Key   string
-			Value int
+	var sortedStruct []keyValue
+
+	for key, value := range cach {
+		sortedStruct = append(sortedStruct, keyValue{key, value})
+	}
+
+	sort.Slice(sortedStruct, func(i, j int) bool {
+		if sortedStruct[i].Value == sortedStruct[j].Value {
+			return sortedStruct[i].Key < sortedStruct[j].Key
 		}
+		return sortedStruct[i].Value > sortedStruct[j].Value
 
-		var sortedStruct []keyValue
+	})
 
-		for key, value := range cach {
-			sortedStruct = append(sortedStruct, keyValue{key, value})
-		}
-
-		sort.Slice(sortedStruct, func(i, j int) bool {
-			if sortedStruct[i].Value == sortedStruct[j].Value {
-				return sortedStruct[i].Key < sortedStruct[j].Key
-			} else {
-				return sortedStruct[i].Value > sortedStruct[j].Value
-			}
-		})
-
-		for i := 0; i < 10; i++ {
+	if len(sortedStruct) < 10 {
+		for i := 0; i < len(sortedStruct); i++ {
 			st = append(st, sortedStruct[i].Key)
 		}
 		return st
 	}
+	for i := 0; i < 10; i++ {
+		st = append(st, sortedStruct[i].Key)
+	}
+	return st
 }
